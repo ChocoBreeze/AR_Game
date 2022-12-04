d6a0aba1e519c9af6c93e56e73035dd4398cbf52
ChocoBreeze <ds030337@gmail.com>
2022-12-04 17:19:33 +0900

    Project Start
    22.12.04

---
+++ Assets/PlaneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneManager : MonoBehaviour
+{
+    public bool isActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
